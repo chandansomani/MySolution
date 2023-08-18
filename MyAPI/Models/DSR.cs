64b52d0e642ecf5dbd766a3fccf15d24e3e9ddc4
Chandan Somani <chandansomani@gmail.com>
2023-08-18 11:09:33 +0530

    Added DSR Model

---
+++ MyAPI/Models/DSR.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyAPI.Models
+{
+    public class DSR
+    {
+        [Key]
+        public int ID { get; set; }
+        [MaxLength(10)]
+        public string BType { get; set; }
+        public DateOnly Date { get; set; }
+        public int BNo { get; set; }
+        public decimal Amount { get; set; }
+        public decimal CreditAmt { get; set; }
+        public decimal UPIAmt { get; set;}
+        public decimal CashAmt { get; set;}
+
+    }
+}
