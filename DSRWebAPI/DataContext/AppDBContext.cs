321fb8688fa13d9cba31aaf897f1181099fafad1
Chandan Somani <chandansomani@gmail.com>
2023-08-21 16:36:50 +0530

    Add Models Set DataContext

---
+++ DSRWebAPI/DataContext/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DSRWebAPI.Models;
+
+namespace DSRWebAPI.DataContext
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options ) : base(options)
+        {
+            
+        }
+
+        public DbSet<CustomerModel> Customers { get; set; }
+        public DbSet<InvoiceModel> Invoices { get; set; }
+    }
+}
