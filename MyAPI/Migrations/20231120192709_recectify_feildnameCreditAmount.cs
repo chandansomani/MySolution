0c5d0693093ec015bda0851c5d4431452254d6ec
Chandan Somani <chandansomani@gmail.com>
2023-11-21 01:02:05 +0530

    Added Migration Update Database Rectified CreditAmount Feild Name

---
+++ MyAPI/Migrations/20231120192709_recectify_feildnameCreditAmount.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class recectify_feildnameCreditAmount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CredfitAmount",
+                table: "Invoice",
+                newName: "CreditAmount");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreditAmount",
+                table: "Invoice",
+                newName: "CredfitAmount");
+        }
+    }
+}
